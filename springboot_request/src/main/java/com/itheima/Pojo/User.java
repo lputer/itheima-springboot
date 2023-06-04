3045c42d121b0298c5e2e7842fbfe2801ac70846
MinValue <1622022466@qq.com>
2023-06-04 17:03:18 +0800

    springboot

---
+++ springboot_request/src/main/java/com/itheima/Pojo/User.java
@@ -0,0 +1,14 @@
+package com.itheima.Pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private Integer age; // 包装类型能接受空null值
+    private Address address;
+}
--- springboot_request/src/main/java/com/itheima/controller/RequestController.java
+++ springboot_request/src/main/java/com/itheima/controller/RequestController.java
@@ -1,10 +1,13 @@
 package com.itheima.controller;
 
+import com.itheima.Pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class RequestController {
@@ -29,6 +32,41 @@ public class RequestController {
     @RequestMapping("simpleParam3")
     public String simpleParam3(@RequestParam("name") String username, Integer age) {
         System.out.println(username + " : " + age);
+        return "OK";
+    }
+
+    // @RequestParam: 参数名和形参名不一致的时候使用该注解做关系映射, 该注解一旦使用就必须要传递参数, 如果不想传递可以设置required
+    @RequestMapping("simpleParam4")
+    public String simpleParam4(@RequestParam(value = "name", required = false) String username, Integer age) {
+        System.out.println(username + " : " + age);
+        return "OK";
+    }
+
+    // 简单pojo
+    @RequestMapping("/simplePojo")
+    public String simplePojo(User user) {
+        System.out.println(user);
+        return "OK";
+    }
+
+    // 复杂pojo
+    @RequestMapping("/complexPojo")
+    public String complexPojo(User user) {
+        System.out.println(user);
+        return "OK";
+    }
+
+    // 数组参数
+    @RequestMapping("/arrayParam")
+    public String arrayParam(String[] hobby) {
+        System.out.println(Arrays.toString(hobby));
+        return "OK";
+    }
+
+    // 集合参数
+    @RequestMapping("/listParam")
+    public String listParam(@RequestParam List hobby) {
+        System.out.println(hobby);
         return "OK";
     }
 }
